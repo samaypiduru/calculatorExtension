5cd808b8348ee452ef50c3bb76c09e0c04fb07fe
samaypiduru <67844360+samaypiduru@users.noreply.github.com>
2021-05-11 13:21:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,64 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const display = document.getElementById('display');
+    const inputKeys = document.getElementsByClassName('inputKey');
 
-let display = document.getElementById('display');
-const inputKeys = document.getElementsByClassName('inputKey');
-
-// adds in event listeners for each input key
-for(let i = 0; i < inputKeys.length; i++) {
-    inputKeys[i].onclick = () => addToExpression(inputKeys[i].value);
-}
-
-// deletes last entered item from the display
-document.getElementById('backspace').onclick = () => {
-    display.value == 'ERR' ? display.value = '' : display.value = display.value.substring(0, display.value.length-1);
-}
-
-// clears the display
-document.getElementById('clear').onclick = () => {
-    display.value = '';
-}
-
-// validates result, rounds to 5 decimals, converts to exponential notation if needed
-document.getElementById('equals').onclick = () => {
-    if(display.value == '') return;
-
-    let result;
-    try {
-        result = eval(display.value);
+    // adds in event listeners for each input key
+    for (let i = 0; i < inputKeys.length; i++) {
+        inputKeys[i].onclick = () => addToExpression(inputKeys[i].value);
     }
-    catch(err) {
-        console.log(err);
-        display.value = "ERR";
-        return;
+
+    // deletes last entered item from the display
+    document.getElementById('backspace').onclick = () => {
+        display.value = (display.value == 'ERR' ? '' : display.value.substring(0, display.value.length-1));
     }
-    if(isInvalid(result)) {
-        display.value = 'ERR';
-        return;
+
+    // clears the display
+    document.getElementById('clear').onclick = () => {
+        display.value = '';
     }
-    if(decimalPlaces(result) > 5) result = result.toFixed(5).trim("0");
-    (tooLong(result)) ? display.value = result.toExponential(2) : display.value = result;
-}
 
-// adds a new character into the expression
-function addToExpression(newVal) {
-    display.value == 'ERR' ? display.value = newVal : display.value += newVal;
-}
+    // validates result, rounds to 5 decimals, converts to exponential notation if needed
+    document.getElementById('equals').onclick = () => {
+        if(display.value == '') return;
 
-// checks if a value is in indeterminate form
-function isInvalid(num) {
-    return (isNaN(num) || num == undefined || num == Infinity);
-}
+        let result;
+        try {
+            result = eval(display.value);
+        } catch(err) {
+            console.log(err);
+            display.value = "ERR";
+            return;
+        }
+    
+        if(isInvalid(result)) {
+            display.value = 'ERR';
+            return;
+        }
+        if(decimalPlaces(result) > 5) result = result.toFixed(5).trim("0");
+        
+        display.value = (tooLong(result) ? result.toExponential(2) : result);
+    }
 
-// returns t/f on if a number is too long and should be converted to exponential notation
-function tooLong(num) {
-    return num.toString().length >= 10;
-}
+    // adds a new character into the expression
+    function addToExpression(newVal) {
+        display.value = (display.value == 'ERR' ? newVal : display.value + newVal);
+    }
 
-// returns the number of decimal places
-function decimalPlaces(num) {
-    return (num % 1) ? num.toString().split('.')[1].length : 0;
-}
+    // checks if a value is in indeterminate form
+    function isInvalid(num) {
+        return (isNaN(num) || num == undefined || num == Infinity);
+    }
 
+    // returns t/f on if a number is too long and should be converted to exponential notation
+    function tooLong(num) {
+        return num.toString().length >= 10;
+    }
 
+    // returns the number of decimal places
+    function decimalPlaces(num) {
+        return (num % 1) ? num.toString().split('.')[1].length : 0;
+    }
 })
